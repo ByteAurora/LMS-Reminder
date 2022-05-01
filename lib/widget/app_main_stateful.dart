b8a9da84085a3a1fb775d8fbd8ec6b6ad8093783
ILoveGameCoding <sonkim1001@naver.com>
2022-05-02 02:28:32 +0900

    모든 과제, 미제출된 과제, 제출된 과제 페이지에 해당하는 BottomNavigationBar 추가
    - 각 페이지별 Tab label은 과제, 미제출, 제출로 설정

--- lib/widget/app_main_stateful.dart
+++ lib/widget/app_main_stateful.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:lms_reminder/widget/all_assignment_page.dart';
+import 'package:lms_reminder/widget/finished_assignment_page.dart';
+import 'package:lms_reminder/widget/unfinished_assignment_page.dart';
 
 /// 최상위 Stateful Widget.
 class AppMainStateful extends StatefulWidget {
@@ -11,14 +14,68 @@ class AppMainStateful extends StatefulWidget {
   State<AppMainStateful> createState() => _AppMainStatefulState();
 }
 
-class _AppMainStatefulState extends State<AppMainStateful> {
+class _AppMainStatefulState extends State<AppMainStateful>
+    with SingleTickerProviderStateMixin {
+  final pages = <Widget>[
+    const AllAssignmentPage(),
+    const UnfinishedAssignmentPage(),
+    const FinishedAssignmentPage()
+  ];
+
+  final tabs = <Tab>[
+    const Tab(
+      icon: Icon(Icons.assignment),
+      text: '과제',
+      iconMargin: EdgeInsets.only(bottom: 4),
+    ),
+    const Tab(
+      icon: Icon(Icons.assignment_late),
+      text: '미제출',
+      iconMargin: EdgeInsets.only(bottom: 4),
+    ),
+    const Tab(
+      icon: Icon(Icons.assignment_turned_in_rounded),
+      text: '제출',
+      iconMargin: EdgeInsets.only(bottom: 4),
+    ),
+  ];
+
+  TabController? tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    tabController!.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.appBarTitle!),
       ),
-      body: Container(),
+      body: TabBarView(
+        children: pages,
+        controller: tabController,
+      ),
+      bottomNavigationBar: Material(
+        color: Colors.blue[700],
+        child: SizedBox(
+          height: 64.0,
+          child: TabBar(
+            tabs: tabs,
+            labelColor: Colors.white,
+            unselectedLabelColor: Colors.grey[400],
+            controller: tabController,
+          ),
+        ),
+      ),
     );
   }
 }
