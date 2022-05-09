4cec6f3a1288fafef62eee6b61476c0125d76bfa
Sohn Young Jin <sonkim1001@naver.com>
2022-05-10 01:41:09 +0900

    과제페이지 파싱 테스트용 코드 추가

---
+++ test/html_assignment_parsing.dart
@@ -0,0 +1,56 @@
+import 'dart:core';
+import 'dart:io';
+
+import 'package:html/dom.dart' as html_dom;
+import 'package:html/parser.dart' as html_parser;
+import 'package:intl/intl.dart';
+import 'package:lms_reminder/model/assignment.dart';
+
+/// Html에서 Assignment 관련 정보 파싱 테스트.
+Future<void> main() async {
+  // 테스트할 과제목록 html 경로
+  String filePath =
+      'D:\\lms_reminder\\others\\LMS page html sample\\LMS 과제정보 - full html code.html';
+
+  String htmlContents = await File(filePath).readAsString();
+
+  html_dom.Document document = html_parser.parse(htmlContents);
+
+  Assignment assignment = Assignment();
+
+  assignment.title = document
+      .getElementById('region-main')!
+      .getElementsByTagName('h2')[0]
+      .text;
+
+  document
+      .getElementById('region-main')!
+      .getElementsByTagName('div')
+      .forEach((element) {
+    if (element.className == 'box generalbox boxaligncenter') {
+      assignment.content = element.text;
+    } else if (element.className == 'submissionstatustable') {
+      element
+          .getElementsByTagName('div')[0]
+          .getElementsByTagName('table')[0]
+          .getElementsByTagName('tbody')[0]
+          .getElementsByTagName('tr')
+          .forEach((element2) {
+        if (element2.getElementsByTagName('td')[0].text.contains("제출 여부")) {
+          assignment.submitState = (element2.text == "제출 완료");
+        } else if (element2
+            .getElementsByTagName('td')[0]
+            .text
+            .contains("종료 일시")) {
+          assignment.deadLine = DateFormat('yyyy-MM-dd HH:mm:ss')
+              .parse(element2.text.replaceAll("종료 일시", "").trim() + ":00");
+        } else if (element2
+            .getElementsByTagName('td')[0]
+            .text
+            .contains("마감까지 남은 기한")) {
+          assignment.leftTime = element2.text.replaceAll("마감까지 남은 기한", "").trim();
+        }
+      });
+    }
+  });
+}
