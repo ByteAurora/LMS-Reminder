a60a48ebfc8b592f1af7d856b6aab9f4366c5688
Sohn Young Jin <sonkim1001@naver.com>
2022-05-13 15:15:13 +0900

    parseLecturesFromHtml 함수 수정

--- lib/model/lecture.dart
+++ lib/model/lecture.dart
@@ -22,6 +22,58 @@ class Lecture {
     List<Lecture> lectureList = List.empty(growable: true);
 
     html_dom.Document document = html_parser.parse(html);
+    document
+        .getElementById('region-main')!
+        .getElementsByTagName('div')[0]
+        .getElementsByClassName('course-content')[0]
+        .getElementsByClassName('total_sections')[0]
+        .getElementsByClassName('course_box')[0]
+        .getElementsByClassName('weeks ubsweeks')[0]
+        .getElementsByTagName('li')
+        .forEach((element) {
+      if (element.className.contains("section main clearfix")) {
+        Lecture lecture = Lecture();
+
+        String sectionName =
+            element.getElementsByClassName('hidden sectionname')[0].text;
+        lecture.week = sectionName.substring(0, sectionName.indexOf(" "));
+        lecture.date = sectionName.substring(
+            sectionName.indexOf("[") + 1, sectionName.indexOf("]"));
+
+        List<Assignment> assignmentList = List.empty(growable: true);
+
+        List<html_dom.Element> activities = element
+            .getElementsByClassName('content')[0]
+            .getElementsByClassName('section img-text');
+
+        if (activities.isNotEmpty) {
+          activities[0]
+              .getElementsByClassName('activity assign modtype_assign ')
+              .forEach((element2) {
+            Assignment assignment = Assignment();
+
+            html_dom.Element assignmentElement = element2
+                .getElementsByTagName('div')[0]
+                .getElementsByClassName('mod-indent-outer')[0]
+                .getElementsByTagName('div')[1]
+                .getElementsByClassName('activityinstance')[0];
+
+            assignment.title = assignmentElement
+                .getElementsByTagName('a')[0]
+                .getElementsByClassName('instancename')[0]
+                .text;
+            assignment.url = assignmentElement.innerHtml.substring(
+                assignmentElement.innerHtml.indexOf("href=") + 6,
+                assignmentElement.innerHtml.indexOf("\">")).replaceAll('https://learn.hoseo.ac.kr', '');
+
+            assignmentList.add(assignment);
+          });
+        }
+        lecture.assignmentList = assignmentList;
+
+        lectureList.add(lecture);
+      }
+    });
 
     return lectureList;
   }
