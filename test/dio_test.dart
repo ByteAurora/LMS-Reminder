4eca013526c9c497c46b364987a4a41ea7a1d4ae
Sohn Young Jin <sonkim1001@naver.com>
2022-05-14 17:25:38 +0900

    테스트용 코드 파일인 dio_test에 LMS 데이터별 불러오는데 걸리는 시간 출력 동작 추가

--- test/dio_test.dart
+++ test/dio_test.dart
@@ -1,15 +1,35 @@
+import 'dart:developer';
+
 import 'package:lms_reminder/manager/lms_manager.dart';
 
 Future<void> main() async {
   LmsManager lmsManager = LmsManager();
 
-  print(await lmsManager.login('20181179', 'dudwls1234'));
+  var stopWatch = Stopwatch();
 
+  stopWatch.start();
+  print(await lmsManager.login('학번', '비밀번호'));
+  print('로그인: ' + stopWatch.elapsed.inMilliseconds.toString());
+
+  stopWatch.reset();
+  stopWatch.start();
   await lmsManager.getCourseList();
+  print('강좌 불러오기: ' + stopWatch.elapsed.inMilliseconds.toString() + "ms");
 
+  stopWatch.reset();
+  stopWatch.start();
   await lmsManager.getLectureList();
+  print('강의 불러오기: ' + stopWatch.elapsed.inMilliseconds.toString() + "ms");
 
+  stopWatch.reset();
+  stopWatch.start();
   await lmsManager.getAssignmentList();
+  print('과제 불러오기: ' + stopWatch.elapsed.inMilliseconds.toString() + "ms");
+
+  stopWatch.reset();
+  stopWatch.start();
+  await lmsManager.getVideoList();
+  print('영상 불러오기: ' + stopWatch.elapsed.inMilliseconds.toString() + "ms");
 
   for (var course in lmsManager.courseList) {
     print(
@@ -21,6 +41,12 @@ Future<void> main() async {
         print('             - ${assignment.submitState ? '제출완료' : '미제출'}');
         print('             - ${assignment.content}');
         print('             - ${assignment.leftTime}');
+      }
+      for (var video in lecture.videoList) {
+        print('       [영상] ${video.title}');
+        print('             - ${video.watchState ? '출석' : '결석'}');
+        print('             - ${video.requiredWatchTime}');
+        print('             - ${video.totalWatchTime}');
       }
     }
   }
