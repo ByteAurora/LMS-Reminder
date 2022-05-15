ded49fa5006b49e2fa176de3ff1c63ce5fc140a2
Sohn Young Jin <sonkim1001@naver.com>
2022-05-15 17:53:44 +0900

    Video 클래스에 영상 시청가능 시간 멤버변수 추가 및 미시청 영상이 있을 경우 생기는 오류 수정

--- lib/model/video.dart
+++ lib/model/video.dart
@@ -6,14 +6,20 @@ class Video {
   /// 영상 제목.
   String? _title;
 
+  /// 시청 상태.
+  bool? _watch;
+
   /// 출석인정 요구시간.
   String? _requiredWatchTime;
 
   /// 총 시청시간.
   String? _totalWatchTime;
 
-  /// 시청 상태.
-  bool? _watchState;
+  /// 활성화 시간.
+  DateTime? _enableTime;
+
+  /// 출석마감 시간.
+  DateTime? _deadLine;
 
   String get title => _title!;
 
@@ -23,10 +29,10 @@ class Video {
 
   String get requiredWatchTime => _requiredWatchTime!;
 
-  bool get watchState => _watchState!;
+  bool get watch => _watch!;
 
-  set watchState(bool value) {
-    _watchState = value;
+  set watch(bool value) {
+    _watch = value;
   }
 
   String get totalWatchTime => _totalWatchTime!;
@@ -37,6 +43,13 @@ class Video {
 
   set requiredWatchTime(String value) {
     _requiredWatchTime = value;
+  }
+
+
+  DateTime get enableTime => _enableTime!;
+
+  set enableTime(DateTime value) {
+    _enableTime = value;
   }
 
   /// 전달된 html에서 영상 정보를 추출하여 반환하는 함수.
@@ -69,10 +82,16 @@ class Video {
           Video video = Video();
           video.title = tdList[1].text;
           video.requiredWatchTime = tdList[2].text;
-          video.totalWatchTime = tdList[3]
-              .innerHtml
-              .substring(0, tdList[3].innerHtml.indexOf('<'));
-          video.watchState = tdList[4].text.contains('O');
+          video.watch = tdList[4].text.contains('O');
+
+          if (video.watch) {
+            video.totalWatchTime = tdList[3]
+                .innerHtml
+                .substring(0, tdList[3].innerHtml.indexOf('<'));
+          } else {
+            video.totalWatchTime = "O0:00";
+          }
+
           weekVideoList!.add(video);
 
           if (currentRowCount == maxRowCount) {
@@ -85,10 +104,16 @@ class Video {
         Video video = Video();
         video.title = tdList[0].text;
         video.requiredWatchTime = tdList[1].text;
-        video.totalWatchTime = tdList[2]
-            .innerHtml
-            .substring(0, tdList[2].innerHtml.indexOf('<'));
-        video.watchState = tdList[3].text.contains('O');
+        video.watch = tdList[3].text.contains('O');
+
+        if (video.watch) {
+          video.totalWatchTime = tdList[2]
+              .innerHtml
+              .substring(0, tdList[2].innerHtml.indexOf('<'));
+        } else {
+          video.totalWatchTime = "O0:00";
+        }
+
         weekVideoList!.add(video);
 
         currentRowCount++;
@@ -100,5 +125,11 @@ class Video {
     });
 
     return courseVideoList;
+  }
+
+  DateTime get deadLine => _deadLine!;
+
+  set deadLine(DateTime value) {
+    _deadLine = value;
   }
 }
