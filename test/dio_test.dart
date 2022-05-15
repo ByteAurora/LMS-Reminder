b0f47f10433da16e3ca95ecf973e405b9ed4a613
Sohn Young Jin <sonkim1001@naver.com>
2022-05-15 17:57:54 +0900

    LmsManager Singleton 패턴 적용 및 Video, Assignment의 기한 표시

--- test/dio_test.dart
+++ test/dio_test.dart
@@ -1,49 +1,50 @@
 import 'package:lms_reminder/manager/lms_manager.dart';
 
 Future<void> main() async {
-  LmsManager lmsManager = LmsManager();
-
   var stopWatch = Stopwatch();
 
   stopWatch.start();
-  print(await lmsManager.login('학번', '비밀번호'));
+  print(await LmsManager().login('20181179', 'dudwls1234'));
   print('로그인: ' + stopWatch.elapsed.inMilliseconds.toString() + "ms");
 
   stopWatch.reset();
   stopWatch.start();
-  await lmsManager.getCourseList();
+  await LmsManager().getCourseList();
   print('강좌 불러오기: ' + stopWatch.elapsed.inMilliseconds.toString() + "ms");
 
   stopWatch.reset();
   stopWatch.start();
-  await lmsManager.getLectureList();
+  await LmsManager().getLectureList();
   print('강의 불러오기: ' + stopWatch.elapsed.inMilliseconds.toString() + "ms");
 
   stopWatch.reset();
   stopWatch.start();
-  await lmsManager.getAssignmentList();
+  await LmsManager().getAssignmentList();
   print('과제 불러오기: ' + stopWatch.elapsed.inMilliseconds.toString() + "ms");
 
   stopWatch.reset();
   stopWatch.start();
-  await lmsManager.getVideoList();
+  await LmsManager().getVideoList();
   print('영상 불러오기: ' + stopWatch.elapsed.inMilliseconds.toString() + "ms");
 
-  for (var course in lmsManager.courseList) {
+  for (var course in LmsManager().courseList) {
     print(
         '[과목] ${course.title}[${course.classNumber}] (교수: ${course.professor})');
     for (var lecture in course.lectureList) {
       print('   [주차] ${lecture.week}(${lecture.date})');
       for (var assignment in lecture.assignmentList) {
         print('       [과제] ${assignment.title}');
-        print('             - ${assignment.submitState ? '제출완료' : '미제출'}');
-        print('             - ${assignment.gradeState}');
+        print('             - ${assignment.submit ? '제출완료' : '미제출'}');
+        print('             - ${assignment.deadLine.toString()}');
+        print('             - ${assignment.grade}');
         print('             - ${assignment.content}');
         print('             - ${assignment.leftTime}');
       }
       for (var video in lecture.videoList) {
         print('       [영상] ${video.title}');
-        print('             - ${video.watchState ? '출석' : '결석'}');
+        print('             - ${video.watch ? '출석' : '결석'}');
+        print('             - ${video.enableTime.toString()}');
+        print('             - ${video.deadLine.toString()}');
         print('             - ${video.requiredWatchTime}');
         print('             - ${video.totalWatchTime}');
       }
