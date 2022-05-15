b12e331ab716faca183c0272ccac9fb012e92729
Sohn Young Jin <sonkim1001@naver.com>
2022-05-15 17:57:26 +0900

    멤버변수명 변경 및 LmsManager Singleton 패턴 적용

--- lib/model/assignment.dart
+++ lib/model/assignment.dart
@@ -4,8 +4,13 @@ import 'package:html/parser.dart' as html_parser;
 import 'package:intl/intl.dart';
 import 'package:lms_reminder/manager/lms_manager.dart';
 
+import '../manager/dio_manager.dart';
+
 /// 과제 정보를 관리하는 클래스.
 class Assignment {
+  /// 과제 URL.
+  String? _url;
+
   /// 제목.
   String? _title;
 
@@ -13,19 +18,16 @@ class Assignment {
   String? _content;
 
   /// 제출 상태.
-  bool? _submitState;
+  bool? _submit;
 
   /// 채점 상태
-  String? _gradeState;
+  String? _grade;
 
   /// 마감일.
   DateTime? _deadLine;
 
   /// 남은 기한.
   String? _leftTime;
-
-  /// 과제 URL.
-  String? _url;
 
   String get title => _title!;
 
@@ -39,7 +41,7 @@ class Assignment {
     _content = value;
   }
 
-  bool get submitState => _submitState!;
+  bool get submit => _submit!;
 
   String get leftTime => _leftTime!;
 
@@ -53,8 +55,8 @@ class Assignment {
     _deadLine = value;
   }
 
-  set submitState(bool value) {
-    _submitState = value;
+  set submit(bool value) {
+    _submit = value;
   }
 
   String get url => _url!;
@@ -63,18 +65,16 @@ class Assignment {
     _url = value;
   }
 
-  String get gradeState => _gradeState!;
+  String get grade => _grade!;
 
-  set gradeState(String value) {
-    _gradeState = value;
+  set grade(String value) {
+    _grade = value;
   }
 
   /// URL로부터 과제 정보 갱신.
   update(LmsManager lmsManager) async {
-    html_dom.Document document = html_parser.parse((await lmsManager.dioManager!
-            .httpGet(
-                Options(headers: {'cookie': lmsManager.dioManager!.cookie}),
-                url))
+    html_dom.Document document = html_parser.parse((await DioManager()
+            .httpGet(options: Options(), useExistCookie: true, subUrl: url))
         .data
         .toString());
 
@@ -99,14 +99,14 @@ class Assignment {
           String text = element2.getElementsByTagName('td')[0].text;
 
           if (text.contains("제출 여부")) {
-            submitState = (element2.text.contains("제출 완료"));
+            submit = (element2.text.contains("제출 완료"));
           } else if (text.contains("종료 일시")) {
             deadLine = DateFormat('yyyy-MM-dd HH:mm:ss')
                 .parse(element2.text.replaceAll("종료 일시", "").trim() + ":00");
           } else if (text.contains("마감까지 남은 기한")) {
             leftTime = element2.text.replaceAll("마감까지 남은 기한", "").trim();
           } else if (text.contains('채점 상황')) {
-            gradeState = element2.text.replaceAll("채점 상황", "").trim();
+            grade = element2.text.replaceAll("채점 상황", "").trim();
           }
         });
       }
