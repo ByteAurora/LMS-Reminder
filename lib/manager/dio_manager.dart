a9591537e64311893dc14a6a0b4b07b862bd5996
Sohn Young Jin <sonkim1001@naver.com>
2022-05-15 16:37:39 +0900

    DioManager 수정
    - Singleton 패턴 적용
    - httpGet, httpPost 함수의 매개변수로 저장된 쿠키 사용 여부 추가

--- lib/manager/dio_manager.dart
+++ lib/manager/dio_manager.dart
@@ -1,8 +1,15 @@
 import 'package:dio/dio.dart';
 
 class DioManager {
+  static final DioManager _instance = DioManager._constructor();
   Dio? dio;
   String? cookie;
+
+  DioManager._constructor();
+
+  factory DioManager() {
+    return _instance;
+  }
 
   void init(BaseOptions options) {
     dio ??= Dio();
@@ -20,10 +27,17 @@ class DioManager {
     return await dio!.post(subUrl, data: data, options: options);
   }
 
-  Future<Response> httpGet(Options options, String subUrl) async {
+  Future<Response> httpGet(
+      {required Options options,
+      bool useExistCookie = false,
+      required String subUrl}) async {
     if (dio == null) {
       throw Exception(
           'Dio object is not initialized. Use HttpManager.init() to initializer dio object.');
+    }
+
+    if (useExistCookie && cookie != null) {
+      options.headers!['cookie'] = cookie;
     }
 
     return await dio!.get(subUrl, options: options);
