3726ced03dc84148aa67d3b547280b9f9d736894
ILoveGameCoding <sonkim1001@naver.com>
2022-05-16 13:35:59 +0900

    공지사항 파싱 테스트 코드 추가

---
+++ test/html_notice_parsing.dart
@@ -0,0 +1,21 @@
+import 'package:dio/dio.dart';
+import 'package:lms_reminder/manager/dio_manager.dart';
+import 'package:lms_reminder/manager/lms_manager.dart';
+import 'package:html/dom.dart' as html_dom;
+import 'package:html/parser.dart' as html_parser;
+
+Future<void> main() async {
+  await LmsManager().login('20181179', 'dudwls1234');
+  // await LmsManager().refreshAllData();
+
+  // for (var course in LmsManager().courseList) {
+  //   for (var notice in course.noticeList) {
+  //     print(course.title + "/" + notice.title);
+  //   }
+  // }
+  html_dom.Document document = html_parser.parse((await DioManager().httpGet(options: Options(), useExistCookie:true, subUrl: '/course/view.php?id=10375')).data.toString());
+
+  String temp = document.getElementById('section-0')!.getElementsByClassName('content')[0].getElementsByClassName('section img-text')[0].getElementsByClassName('activity ubboard modtype_ubboard')[0].getElementsByTagName('div')[4].innerHtml;
+  
+  print(temp.substring(temp.indexOf('href="https://learn.hoseo.ac.kr') + 'href="https://learn.hoseo.ac.kr'.length, temp.indexOf('">')));
+}
