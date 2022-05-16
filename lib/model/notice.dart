cae83e02deef9b4c55f099fbc96fb29ac8cfb7f1
ILoveGameCoding <sonkim1001@naver.com>
2022-05-16 13:31:23 +0900

    Notice 클래스 업데이트 함수 추가(추가 구현 필요)

--- lib/model/notice.dart
+++ lib/model/notice.dart
@@ -1,4 +1,10 @@
+import 'package:dio/dio.dart';
+
+import '../manager/dio_manager.dart';
 import 'course.dart';
+
+import 'package:html/dom.dart' as html_dom;
+import 'package:html/parser.dart' as html_parser;
 
 /// 공지사항 정보를 관리하는 클래스.
 class Notice {
@@ -19,6 +25,65 @@ class Notice {
 
   /// 날짜.
   String? _date;
+
+  /// 전달된 html에서 영상 정보를 추출하여 반환하는 함수.
+  static Future<List<Notice>> praseNoticeFromHtml(
+      Course course, String html) async {
+    List<Notice> courseNoticeList = List.empty(growable: true);
+
+    html_dom.Document document = html_parser.parse(html);
+
+    document
+        .getElementById('ubboard_list_form')!
+        .getElementsByTagName('table')[0]
+        .getElementsByTagName('tbody')[0]
+        .getElementsByTagName('tr')
+        .forEach((noticeElement) async {
+      Notice notice = Notice(course);
+
+      String temp = noticeElement.getElementsByTagName('td')[1].innerHtml;
+      notice.url = temp.substring(temp.indexOf('kr') + 2, temp.indexOf('">'));
+      notice.title = noticeElement.getElementsByTagName('td')[1].text.trim();
+      notice.author = noticeElement.getElementsByTagName('td')[2].text.trim();
+      notice.date = noticeElement.getElementsByTagName('td')[3].text.trim();
+
+      Response response = await DioManager().httpGet(
+          options: Options(), useExistCookie: true, subUrl: notice.url);
+      html_dom.Document detailDocument =
+          html_parser.parse(response.data.toString());
+
+      notice.content = detailDocument
+          .getElementById('region-main')!
+          .getElementsByTagName('div')[0]
+          .getElementsByClassName('ubboard')[0]
+          .getElementsByTagName('div')[1]
+          .getElementsByClassName('content')[0]
+          .text;
+
+      print(course.title +
+          "/" +
+          notice.url +
+          "/" +
+          notice.title +
+          "/" +
+          notice.author +
+          "/" +
+          notice.date +
+          "/" +
+          notice.content);
+
+      courseNoticeList.add(notice);
+    });
+
+    return courseNoticeList;
+  }
+
+  /// Notice 생성자.
+  Notice(Course? course) {
+    _course = course;
+  }
+
+  Future update() async {}
 
   String get title => _title!;
 
