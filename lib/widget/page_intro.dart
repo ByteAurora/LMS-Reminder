a438fc6e25dc6867d532869c1420ec5f42fc3ba6
Sohn Young Jin <sonkim1001@naver.com>
2022-05-21 19:45:22 +0900

    인트로 화면에서 테스트용 WorkManager 코드 제거

--- lib/widget/page_intro.dart
+++ lib/widget/page_intro.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:workmanager/workmanager.dart';
-import 'package:lms_reminder/main.dart';
 
 class PageIntro extends StatefulWidget {
   const PageIntro({Key? key}) : super(key: key);
@@ -20,10 +18,6 @@ class _PageIntroState extends State<PageIntro> {
             Text('인트로 화면'),
             ElevatedButton(
               onPressed: () {
-                Workmanager().initialize(
-                  callbackDispatcher,
-                  isInDebugMode: true,
-                );
                 Navigator.popAndPushNamed(context, '/tutorial');
               },
               child: Text('튜토리얼 화면 이동'),
